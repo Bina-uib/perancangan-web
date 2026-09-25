ed5bf91431fc2d7d4f18a3a1bdcfb5bdca4e3334
Bina-uib <shabrinaalisya@gmail.com>
2026-09-25 23:03:11 +0700

    JenisKelaminModel.js

--- src/models/jenis_kelamin/jenisKelaminModel.js
+++ src/models/jenis_kelamin/jenisKelaminModel.js
@@ -1,2 +1,34 @@
 const { Datatypes } = require("sequelize");
 const sequelize = require("../../config/database");
+
+const JenisKelamin = sequelize.define(
+  "JenisKelamin",
+  {
+    id_jenis_kelamin: {
+        type: Datatypes.UUID,
+        defaultValue: Datatypes.UUIDV4,
+        primaryKey: true,
+    },
+
+    kode: {
+        type: Datatypes.CHAR(1),
+        allowNull: false,
+        unique: true
+    },
+
+    nama: {
+        type: Datatypes.STRING(20),
+        allowNull: false,
+    },
+
+    create_at: {
+        type: Datatypes.DATE,
+        defaultValue: Datatypes.NOW
+    },
+
+    update_at: {
+        type: Datatypes.DATE,
+        defaultValue: Datatypes.NOW
+    },
+  }
+)
